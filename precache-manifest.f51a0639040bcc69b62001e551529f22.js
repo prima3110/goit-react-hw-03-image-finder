0cd9d8927269dae3dd8df96f6303bdc00dade8e8
Roman Pryymak <prima3110@ukr.net>
2020-01-12 17:30:06 +0200

    Updates

---
+++ precache-manifest.f51a0639040bcc69b62001e551529f22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ae379d1411abf451926bc01e44d1bc",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "f39267caeb9612dfe84b",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "fcaf24b5c533c4998f67",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.30426aaa.chunk.css"
+  },
+  {
+    "revision": "f39267caeb9612dfe84b",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.0ded6a50.chunk.js"
+  },
+  {
+    "revision": "fcaf24b5c533c4998f67",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.f268e0b2.chunk.js"
+  },
+  {
+    "revision": "f32aa933252941e213bb",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.2e0a5410.js"
+  }
+]);
